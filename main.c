3bfea7d8afe9aefcc01472e0bc5d185c8a783973
Noctyris <noctyris@proton.me>
2025-03-22 18:35:58 +0100

    Init (and update)

---
+++ main.c
@@ -0,0 +1,64 @@
+#include "utils.h"
+
+int main(int argc, char* argv[]) {
+    // Init SDL
+    SDL_SetHint(SDL_HINT_RENDER_VSYNC, "0");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+        printf("Error while initialisation");
+        return 1;
+    }
+
+    // Create window
+    SDL_Window* window= SDL_CreateWindow(
+        "Maze",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL) {
+        printf("Error while creating window");
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create renderer for window
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    if (renderer == NULL) {
+        printf("Error while creating renderer");
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Coordinate DIRECTIONS[] = {
+        {0, 1},
+        {0, -1},
+        {1, 0},
+        {-1, 0},
+    };
+
+    maze(renderer,DIRECTIONS);
+
+    // Event loop
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+        draw_sprite(renderer, 0, 0);
+        remove_sprite(renderer, 0, 0);
+        SDL_RenderPresent(renderer);
+    }
+
+    // Quit SDL
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
---
+++ utils.h
@@ -0,0 +1,84 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <math.h>
+
+#define SIZE    10
+#define WIDTH   800
+#define HEIGHT  600
+
+typedef struct {
+    int x;
+    int y;
+} Coordinate;
+
+void draw_filled_circle(SDL_Renderer* renderer, int cx, int cy, int radius, int r, int g, int b) {
+    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+    for (int y = -radius; y <= radius; y++) {
+        int dx = (int)sqrt(radius * radius - y * y);
+        SDL_RenderDrawLine(renderer, cx - dx, cy + y, cx + dx, cy + y);
+    }
+}
+
+Coordinate chooseRandomCoordinate(Coordinate* directions) {
+    int randomIndex = rand() % 4;
+    return directions[randomIndex];
+}
+
+void maze(SDL_Renderer* renderer, Coordinate* directions) {
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+    for (int x = SIZE; x < WIDTH; x += SIZE * 2) {
+        for (int y = SIZE; y < HEIGHT; y += SIZE * 2) {
+            SDL_Rect rect = {x, y, SIZE, SIZE};
+            SDL_RenderFillRect(renderer, &rect);
+
+            Coordinate randomCoord = chooseRandomCoordinate(directions);
+            SDL_Rect adjacentRect = {x + randomCoord.x * SIZE, y + randomCoord.y * SIZE, SIZE, SIZE};
+            SDL_RenderFillRect(renderer, &adjacentRect);
+        }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 200, 50, 255);
+    SDL_RenderFillRect(renderer, &(SDL_Rect){0, 0, SIZE, SIZE});
+
+    SDL_SetRenderDrawColor(renderer, 200, 50, 0, 255);
+    SDL_RenderFillRect(renderer, &(SDL_Rect){WIDTH - SIZE, HEIGHT - SIZE, SIZE, SIZE});
+}
+
+Uint32 get_renderer_pixel(SDL_Renderer* renderer, int x, int y) {
+    Uint32 pixel;
+    SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 32, SDL_PIXELFORMAT_RGBA32);
+    if (SDL_RenderReadPixels(renderer, &(SDL_Rect){x, y, 1, 1}, SDL_PIXELFORMAT_RGBA32, surface->pixels, surface->pitch) != 0) {
+        printf("Erreur SDL_RenderReadPixels: %s\n", SDL_GetError());
+        SDL_FreeSurface(surface);
+        return 0;
+    }
+
+    pixel = *(Uint32 *)surface->pixels;
+    SDL_FreeSurface(surface);
+    return pixel;
+}
+
+void get_rgba(Uint32 pixel, SDL_PixelFormat *format, Uint8 *r, Uint8 *g, Uint8 *b, Uint8 *a) {
+    SDL_GetRGBA(pixel, format, r, g, b, a);
+}
+
+void draw_sprite(SDL_Renderer* renderer, int x, int y) {
+    draw_filled_circle(renderer, x + SIZE / 2 - 1, y + SIZE / 2 - 1, 3, 255, 255, 0);
+}
+
+void remove_sprite(SDL_Renderer* renderer, int x, int y) {
+    Uint32 pixel = get_renderer_pixel(renderer, x, y);
+    Uint8 r, g, b, a;
+
+    // Récupérer le format depuis le renderer
+    SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 32, SDL_PIXELFORMAT_RGBA32);
+    get_rgba(pixel, surface->format, &r, &g, &b, &a);
+    SDL_FreeSurface(surface);
+
+    printf("Pixel à (%d, %d) -> RGBA: (%d, %d, %d, %d)\n", x, y, r, g, b, a);
+}
+
+#endif
