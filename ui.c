533aac36a246c807560033b62a6ebd6e3f591f56
noctyris <noctyris@proton.me>
2025-04-14 20:21:22 +0200

    Refactored draw_sprite and remove_sprite functions

--- ui.c
+++ ui.c
@@ -26,14 +26,6 @@ void maze(SDL_Renderer* renderer, Coordinate* directions) {
     SDL_RenderFillRect(renderer, &(SDL_Rect){WIDTH*SIZE - SIZE, HEIGHT*SIZE - SIZE, SIZE, SIZE});
 }
 
-void draw_filled_circle(SDL_Renderer* renderer, int cx, int cy, int radius, int r, int g, int b) {
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-    for (int y = -radius; y <= radius; y++) {
-        int dx = (int)sqrt(radius * radius - y * y);
-        SDL_RenderDrawLine(renderer, cx - dx, cy + y, cx + dx, cy + y);
-    }
-}
-
 Uint32 get_renderer_pixel(SDL_Renderer* renderer, int x, int y) {
     Uint32 pixel;
     SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 32, SDL_PIXELFORMAT_RGBA32);
@@ -53,17 +45,16 @@ void get_rgba(Uint32 pixel, SDL_PixelFormat *format, Uint8 *r, Uint8 *g, Uint8 *
 }
 
 void draw_sprite(SDL_Renderer* renderer, int x, int y) {
-    draw_filled_circle(renderer, x + SIZE / 2 - 1, y + SIZE / 2 - 1, SIZE / 4, 255, 255, 0);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_RenderFillRect(renderer, &(SDL_Rect){x, y, SIZE, SIZE});
 }
 
 void remove_sprite(SDL_Renderer* renderer, int x, int y) {
-    Uint32 pixel = get_renderer_pixel(renderer, x, y);
-    Uint8 r, g, b, a;
-    SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 32, SDL_PIXELFORMAT_RGBA32);
-    get_rgba(pixel, surface->format, &r, &g, &b, &a);
-    SDL_FreeSurface(surface);
-
-    draw_filled_circle(renderer, x + SIZE / 2 - 1, y + SIZE / 2 - 1, SIZE / 4, r, g, b);
+    int r = 30, g = 30, b = 30;
+    if (x == 0 && y == 0) r = 0, g = 200, b = 50;
+    else if (x == WIDTH * SIZE - SIZE && y == HEIGHT * SIZE - SIZE) r = 200, g = 50, b = 0;
+    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+    SDL_RenderFillRect(renderer, &(SDL_Rect){x, y, SIZE, SIZE});
 }
 
 int isCellOk(SDL_Renderer* renderer, int x, int y) {
